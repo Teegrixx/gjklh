2d5497fe02e8016dd65563daf272302323e85112
teegrixx <89885361+Teegrixx@users.noreply.github.com>
2024-04-20 15:13:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from img2pdf.core import fld2pdf, fld2thumb
 from img2tph.core import img2tph
 from plugins import MangaClient, ManhuaKoClient, MangaCard, MangaChapter, ManhuaPlusClient, TMOClient, MangaDexClient, \
     MangaSeeClient, MangasInClient, McReaderClient, MangaKakalotClient, ManganeloClient, ManganatoClient, \
-    KissMangaClient, MangatigreClient, MangaHasuClient, MangaBuddyClient, AsuraScansClient, NineMangaClient, ComickIOClient
+    KissMangaClient, MangatigreClient, MangaHasuClient, MangaBuddyClient, AsuraScansClient, NineMangaClient, ComicKioClient
 import os
 
 from pyrogram import Client, filters
